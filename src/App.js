afcf5dbe8d506b14f5daea2224dc22bdcf2a3674
Serhii Karnaukh <knauch@protonmail.com>
2022-06-21 10:04:10 +0200

    +

--- src/App.js
+++ src/App.js
@@ -3,8 +3,63 @@ const App = () => {
 
   return (
 
-    <div>
-      Hui ebanie
+    <div className="categories-container">
+      
+      {/*#1 product category*/}
+      <div className="category-container">
+        {/*<img />  */}
+
+        <div className="category-body-container">
+          <h2>Caps and stuff</h2>
+          <p>Check them OUT!</p>
+        </div>
+
+      </div>
+
+      {/*#2 product catg*/}
+      <div className="category-container">
+        {/*<img />  */}
+
+        <div className="category-body-container">
+          <h2>Jackets or Clifts</h2>
+          <p>Check them OUT!</p>
+        </div>
+
+      </div>
+
+      {/*#3 product catg*/}
+      <div className="category-container">
+        {/*<img />  */}
+
+        <div className="category-body-container">
+          <h2>BOOTS!</h2>
+          <p>Check them OUT!</p>
+        </div>
+
+      </div>
+
+      {/*#4 product catg*/}
+      <div className="category-container">
+        {/*<img />  */}
+
+        <div className="category-body-container">
+          <h2>Chicks girly stuff</h2>
+          <p>Check them OUT!</p>
+        </div>
+
+      </div>
+
+      {/*#5 product catg*/}
+      <div className="category-container">
+        {/*<img />  */}
+
+        <div className="category-body-container">
+          <h2>The DUDES land</h2>
+          <p>Check them OUT!</p>
+        </div>
+
+      </div>
+      
     </div>
   )
 
