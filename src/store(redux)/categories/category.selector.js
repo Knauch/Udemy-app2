b79e471be1d072ceedb6a8fd44ea60fccf222218
Serhii Karnaukh <skarnaukh@bihl-wiedemann.de>
2022-11-29 09:11:46 +0100

    memoiz func added to category select

--- src/store(redux)/categories/category.selector.js
+++ src/store(redux)/categories/category.selector.js
@@ -1,3 +1,17 @@
+import { createSelector } from 'reselect';
+
+//input Selectors - giving us the parameters for the output
+
+//initial selector for first data
+const selectCategoryReducer = (state) => state.categories;
+
+//memoizaition function that takes 2 arguments 1st - input and 2nd - output
+export const selectCategories = createSelector(
+  [selectCategoryReducer], 
+  (categoriesSlice) => categoriesSlice.categories
+)
+
+
 export const selectCategoriesMap = (state) => 
 state.categories.categories.reduce( (acc, category) => {
     const { title, items} = category;
